8f971425001f4bbf717309e2e24b2029a336ca7b
coleb <cole.ballard97@gmail.com>
2021-04-28 16:51:05 -0600

    Create basic_data_types.cpp

---
+++ basic_data_types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char ch;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &ch, &f, &d);
+    cout << i << endl;
+    cout << l << endl;
+    cout << ch << endl;
+    cout << setprecision(3) << fixed << f << endl;
+    cout << setprecision(9) << fixed << d << endl;
+    
+    return 0;
+}
